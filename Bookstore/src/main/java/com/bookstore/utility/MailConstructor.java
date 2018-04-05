4cf22ecf504a08aa16b6fa25dba3d04b941e3775
Jorge Concepcion <jorge_concepcion@ultimatesoftware.com>
2018-04-04 20:07:12 -0400

    Adding rehusable email

--- Bookstore/src/main/java/com/bookstore/utility/MailConstructor.java
+++ Bookstore/src/main/java/com/bookstore/utility/MailConstructor.java
@@ -21,32 +21,32 @@ import com.bookstore.domain.User;
 public class MailConstructor {
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private TemplateEngine templateEngine;
-	
+
 	public SimpleMailMessage constructResetTokenEmail(
 			String contextPath, Locale locale, String token, User user, String password
 			) {
-		
+
 		String url = contextPath + "/newUser?token="+token;
 		String message = "\nPlease click on this link to verify your email and edit your personal information. Your password is: \n"+password;
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setTo(user.getEmail());
-		email.setSubject("Le's Bookstore - New User");
+		email.setSubject("Welcome to Tealy Bookstore - New User");
 		email.setText(url+message);
 		email.setFrom(env.getProperty("support.email"));
 		return email;
-		
+
 	}
-	
+
 	public MimeMessagePreparator constructOrderConfirmationEmail (User user, Order order, Locale locale) {
 		Context context = new Context();
 		context.setVariable("order", order);
 		context.setVariable("user", user);
 		context.setVariable("cartItemList", order.getCartItemList());
 		String text = templateEngine.process("orderConfirmationEmailTemplate", context);
-		
+
 		MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
 			@Override
 			public void prepare(MimeMessage mimeMessage) throws Exception {
@@ -54,10 +54,10 @@ public class MailConstructor {
 				email.setTo(user.getEmail());
 				email.setSubject("Order Confirmation - "+order.getId());
 				email.setText(text, true);
-				email.setFrom(new InternetAddress("ray.deng83@gmail.com"));
+				email.setFrom(new InternetAddress("tealy.bookstore@gmail.com"));
 			}
 		};
-		
+
 		return messagePreparator;
 	}
 }
